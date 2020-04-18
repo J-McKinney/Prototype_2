440df4192f4d17ab0bb149b97cc158fd81b78c2f
Jesse McKinney <jessman51386@gmail.com>
2020-04-18 16:28:43 -0400

    yay

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/LandingPage/Landing";
-import Gameplay from "./pages/Gameplay/Gameplay";
-import Instructions from "./pages/Instructions/Instructions";
-import License from "./pages/License/License";
+// import Gameplay from "./pages/Gameplay/Gameplay";
+// import Instructions from "./pages/Instructions/Instructions";
+// import License from "./pages/License/License";
 import Dictaphone from "./components/SpeechRecognition/SpeechRecognition";
 import ErrorPage from "./pages/ErrorPage/Error";
 
@@ -12,10 +12,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/gameplay" component={Gameplay} />
         <Route exact path="/game" component={Dictaphone} />
-        <Route exact path="/instructions" component={Instructions} />
-        <Route exact path="/license" component={License} />
         <Route exact path="*" component={ErrorPage} />
       </Switch>
     </Router>
