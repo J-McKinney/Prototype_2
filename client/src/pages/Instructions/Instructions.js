f0e9d5c4ffae5202f0cdb396db51e600d59eb309
Jesse McKinney <jessman51386@gmail.com>
2020-04-18 16:50:48 -0400

    going for a walk

--- client/src/pages/Instructions/Instructions.js
+++ client/src/pages/Instructions/Instructions.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import "./Instructions.css";
 
 class Instructions extends Component {
